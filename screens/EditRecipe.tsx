277fea6d2bf1bf9727d159c8deacda8904092a34
MatsCornegoor <m.cornegoor@gmail.com>
2026-04-06 15:16:24 +0200

    added background to recipe scroll container

--- screens/EditRecipe.tsx
+++ screens/EditRecipe.tsx
@@ -8,10 +8,12 @@ import { NestableScrollContainer } from 'react-native-draggable-flatlist';
 import ContentWrapper from '../components/ContentWrapper';
 import RecipeStore from '../store/RecipeStore';
 import { Recipe } from '../models/Recipe';
+import { useTheme } from '../hooks/useTheme';
 
 export default function EditRecipe() {
   const navigation = useNavigation<any>();
   const route = useRoute<any>();
+  const { colors } = useTheme();
   const { id } = route.params;
   const originalRecipe = RecipeStore.getRecipeById(Array.isArray(id) ? id[0] : id);
 
@@ -26,10 +28,10 @@ export default function EditRecipe() {
   };
 
   return (
-    <GestureHandlerRootView style={{ flex: 1 }}>
-      <View style={{ flex: 1 }}>
+    <GestureHandlerRootView style={{ flex: 1, backgroundColor: colors.background }}>
+      <View style={{ flex: 1, backgroundColor: colors.background }}>
         <Header title="Edit recipe" />
-        <NestableScrollContainer style={{ flex: 1 }}>
+        <NestableScrollContainer style={{ flex: 1, backgroundColor: colors.background }}>
           <ContentWrapper>
             <RecipeForm mode="edit" initialRecipe={originalRecipe} onSave={handleSave} />
           </ContentWrapper>
